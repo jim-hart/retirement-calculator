ff5d7eef0727b3dfce486188898b58ab27c93c00
Jim Hart <24954159+jim-hart@users.noreply.github.com>
2023-12-23 22:35:08 -0600

    Add mising typing

--- tests/test_models.py
+++ tests/test_models.py
@@ -22,12 +22,12 @@ def mock_user(mock_user_data) -> User:
         "pre_retirement_income_percent",
     ),
 )
-def test_written_percentages_converted_to_float(field, mock_user_data):
+def test_written_percentages_converted_to_float(field: str, mock_user_data: dict):
     user = User.model_validate(mock_user_data)
     assert getattr(user, field) == mock_user_data[field] / 100
 
 
-def test_retirement_age_must_be_greater_than_current_age(mock_user_data):
+def test_retirement_age_must_be_greater_than_current_age(mock_user_data: dict):
     date_of_birth = date.fromisoformat(mock_user_data["date_of_birth"])
     delta = date.today() - date_of_birth
     mock_user_data["retirement_age"] = delta.days // 365
@@ -35,30 +35,30 @@ def test_retirement_age_must_be_greater_than_current_age(mock_user_data):
         User.model_validate(mock_user_data)
 
 
-def test_life_expectancy_must_be_greater_than_retirement_age(mock_user_data):
+def test_life_expectancy_must_be_greater_than_retirement_age(mock_user_data: dict):
     mock_user_data["life_expectancy"] = mock_user_data["retirement_age"]
     with pytest.raises(ValidationError):
         User.model_validate(mock_user_data)
 
 
-def test_current_age_matches_expected_value(mock_user_data):
+def test_current_age_matches_expected_value(mock_user_data: dict):
     """
     Verify User.current_age is correctly calculated using User.date_of_birth
     """
     expected_age = 50
-    date_of_birth = date.today() - timedelta(days=(365 * expected_age) + 10)
+    date_of_birth = age_to_date_of_birth(expected_age)
     mock_user_data["date_of_birth"] = date_of_birth.isoformat()
     user = User.model_validate(mock_user_data)
     assert user.current_age == expected_age
 
 
-def test_calculated_years_to_retirement_matches_expected_value(mock_user_data):
+def test_calculated_years_to_retirement_matches_expected_value(mock_user_data: dict):
     user = User.model_validate(mock_user_data)
     expected_years = mock_user_data["retirement_age"] - user.current_age
     assert user.years_to_retirement == expected_years
 
 
-def test_calculated_years_in_retirement_matches_expected_value(mock_user_data):
+def test_calculated_years_in_retirement_matches_expected_value(mock_user_data: dict):
     user = User.model_validate(mock_user_data)
     expected_years = (
         mock_user_data["life_expectancy"] - mock_user_data["retirement_age"]
